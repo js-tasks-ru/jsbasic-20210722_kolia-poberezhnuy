942b613449935cc584fd8f5ba57d801e55751c65
kol_iap <read.me.rj88@gmail.com>
2021-09-03 17:44:59 +0300

    made edits to module 4

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,5 +1,5 @@
 function makeDiagonalRed(table) {
-  for (i = 0; i < table.rows.length; i++) {
+  for (let i = 0; i < table.rows.length; i++) {
     let row = table.rows[i];
     row.cells[i].style.backgroundColor = 'red';
   }
